e551daf8b66823ee223c45151ac8e467bcb7a6c3
David Sledge <sledged@cs.unm.edu>
2016-09-17 13:32:53 -0600

    Reduced idle time between responses to 10 seconds and restuctured idle responses

--- sickbot.py
+++ sickbot.py
@@ -13,6 +13,7 @@ class ListenThread (threading.Thread):
         self.name = name
         self.daemon = True
         self.lastResponseTime = time.time()
+        self.impatienceLevel = 0
     def run(self):
         while True:
             question = cli.read_question()
@@ -23,59 +24,49 @@ class ListenThread (threading.Thread):
                 knowledge_base.transcript.append(question)
                 cli.write_answer(answer)
                 self.lastResponseTime = time.time()
+                self.impatienceLevel = 0
 
-def main():
-    print config.welcome_msg
-    listenThread = ListenThread(1, "Thread-1", 1)
-    listenThread.start()
-    kbResponse = 0;
+class Main:
+    def __init__(self):
+        self.kbResponse = 0
 
-    while True:
-        if not listenThread.isAlive():
-            return
-        timeDiff = time.time() - listenThread.lastResponseTime
-        if timeDiff >= 121:
-            return
-        if timeDiff >= 120:
-            answer = "Bye"
-            nextResponseIn = 121
-        elif timeDiff >= 105:
-            answer = "I'm going to leave"
-            nextResponseIn = 120
-        elif timeDiff >= 90:
-            answer = "Sigh..."
-            nextResponseIn = 105
-        elif timeDiff >= 75:
-            answer = "Hello?"
-            nextResponseIn = 90
-        elif timeDiff >= 60:
-            answer = "Are you still there?"
-            nextResponseIn = 75
-        elif timeDiff >= 45:
-            answer = config.knowledge_base[""][kbResponse]
-            kbResponse += 1
-            if kbResponse >= len(config.knowledge_base[""]):
-                kbResponse = 0
-            nextResponseIn = 60
-        elif timeDiff >= 30:
-            answer = config.knowledge_base[""][kbResponse]
-            kbResponse += 1
-            if kbResponse >= len(config.knowledge_base[""]):
-                kbResponse = 0
-            nextResponseIn = 45
-        elif timeDiff >= 15:
-            answer = config.knowledge_base[""][kbResponse]
-            kbResponse += 1
-            if kbResponse >= len(config.knowledge_base[""]):
-                kbResponse = 0
-            nextResponseIn = 30
-        else:
-            time.sleep(15 - timeDiff)
-            continue
-        cli.write_answer(answer)
-        time.sleep(nextResponseIn - timeDiff)
-    return
+    def query(self, rspHandler):
+        rspHandler(config.knowledge_base[""][self.kbResponse])
+        self.kbResponse += 1
+        if self.kbResponse >= len(config.knowledge_base[""]):
+            self.kbResponse = 0
 
+    def main(self):
+        print config.welcome_msg
+        listenThread = ListenThread(1, "Thread-1", 1)
+        listenThread.start()
+        impatientResponses = [
+            self.query
+          , self.query
+          , self.query
+          , lambda rspHandler:
+                rspHandler("Are you still there?")
+          , lambda rspHandler:
+                rspHandler("Hello?")
+          , lambda rspHandler:
+                rspHandler("Sigh...")
+          , lambda rspHandler:
+                rspHandler("I'm going to leave")
+          , lambda rspHandler:
+                rspHandler("Bye")
+        ]
+
+        while True:
+            if not listenThread.isAlive():
+                return
+            timeDiff = time.time() - listenThread.lastResponseTime
+            if timeDiff / 10 > len(impatientResponses) + 1:
+                return
+            if timeDiff / 10 > listenThread.impatienceLevel + 1:
+                impatientResponses[listenThread.impatienceLevel](cli.write_answer)
+                listenThread.impatienceLevel += 1
+            time.sleep(1)
+        return
 
 if __name__ == "__main__":
-    main()
+    Main().main()
